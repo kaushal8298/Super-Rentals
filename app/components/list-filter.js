23d072f2f1cabd9365cf0769169a5d790df444f2
Kaushal <mkaushal8298@gmail.com>
2017-08-24 21:25:08 +0530

    Synchronized Search Results

--- app/components/list-filter.js
+++ app/components/list-filter.js
@@ -6,14 +6,20 @@ export default Ember.Component.extend({
 
   init() {
     this._super(...arguments);
-    this.get('filter')('').then((results) => this.set('results', results));
+    this.get('filter')('').then((allResults) => {
+      this.set('results', allResults.results);
+    });
   },
 
   actions: {
     handleFilterEntry() {
       let filterInputValue = this.get('value');
       let filterAction = this.get('filter');
-      filterAction(filterInputValue).then((filterResults) => this.set('results', filterResults));
+      filterAction(filterInputValue).then((resultsObj) => {
+        if (resultsObj.query === this.get('value')) {
+          this.set('results', resultsObj.results);
+        }
+      });
     }
   }
 
